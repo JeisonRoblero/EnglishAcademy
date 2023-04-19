3c47ed5b31647a395805899001796e0ca5b098bd
Jeison <jeisonroblero.ga>
2023-04-18 21:02:42 -0600

    Actualización plataforma

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+    setTimeout(function(){
+        const loader = document.querySelector('.loader');
+        loader.style.opacity = 0;
+        loader.style.visibility = 'hidden';
+    }, 0);
+});
+
+
+
